2a9b191a991fec6583d7bbe83db3ff37cb4b5920
Yu-Chieh Liao <tina5ps93@gmail.com>
2022-08-12 12:15:43 +0800

    [Feat] update yaml

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-
-const express = require('express')
-const YAML = require('yamljs')
+const express = require('express');
+const YAML = require('yamljs');
 
 // Swagger
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = YAML.load('./swagger.yml')
+const swaggerDocument = YAML.load('./swagger2.yml');
 // Application
-const app = express()
+const app = express();
 
-app.use(
-    '/api-docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
-  );
-
-app.listen(5000, ()=>{
-    console.log("Creating swagger ui.");
-})
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/', (req, res) => {
+  console.log('ok');
+});
+app.listen(4000, () => {
+  console.log('Creating swagger ui.');
+});
